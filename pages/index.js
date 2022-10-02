f77f8e6d6444cd6d08a9799a9d4d379b7866499a
marko-z <m.zakrzwsk@gmail.com>
2022-10-02 16:06:31 +0200

    grid layout for apps start

--- pages/index.js
+++ pages/index.js
@@ -17,40 +17,56 @@ export default function Index({ entries }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div className="p-12">
+      <div className="p-8 sm:p-24">
         {/* <div style={{width: '30px', borderRadius: '5px', overflow: 'hidden'}}>
           <Image src={'/images/WICKED.png'} layout='fill' objectFit='contain'/>
         </div> */}
-        <div className={styles.container}>
-          <div className={styles.postCard}>
-            <div className={styles.text}>
-              <h1>Marek Zakrzewski</h1>
-              <p>
-                Have a look at my warez
-              </p>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <div className="text-[4rem] sm:text-[6rem]">
+              Marek Zakrzewski
             </div>
-
-            <div className={styles.profileImageContainer}>
-              <Image
-                src="/images/WICKED.png"
-                layout="fill"
-                objectFit="contain"
-              />
+            <div className="text-[2rem] text-gray-500">
+              Developer
             </div>
           </div>
+          <div className="relative w-64 h-64">
+            <Image
+              alt="wicked"
+              src="/images/WICKED.png"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+        
+        <div id="apps">
+        <div className="grid w-fit grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            {entries.map((entry,index) => {
+              return (
+                <div key={index} className="h-60 w-60 rounded-xl flex flex-col shadow-2xl bg-gray-900 overflow-hidden">
+                  <div className="relative w-full h-3/4">
+                    <Image alt='app image' src={entry.metadata.imgsrc} layout="fill" objectFit="cover" />
+                  </div>
+                  <div className="w-full h-1/4 p-4 text-[1rem]">
+                    {entry.metadata.title}
+                  </div>
+                </div>
+              )
+              
+              // <Entry
+              //   key={entry.id}
+              //   title={entry.metadata.title}
+              //   summary={entry.metadata.summary}
+              //   imgsrc={entry.metadata.imgsrc}
+              //   href={`/entries/${entry.id}`}
+              // />
+            })}
         </div>
         <div className={styles.container}>
           <ul className={styles.list}>
-            {entries.map((entry) => {
-              return <Entry
-                key={entry.id}
-                title={entry.metadata.title}
-                summary={entry.metadata.summary}
-                imgsrc={entry.metadata.imgsrc}
-                href={`/entries/${entry.id}`}
-              />
-            })}
           </ul>
+        </div>
         </div>
       </div>
       <footer className={styles.footer}>
@@ -64,8 +80,8 @@ export async function getServerSideProps() {
   // apparently you can't access imported module properties before you've resolved it as a promise
   // this is some time-consuming debugging right here
   const filenames = fs.readdirSync(path.resolve(process.cwd(), 'pages', 'entries'))
-  const metadata = await Promise.all(filenames.map(async (filename) => [await require(`./entries/${filename}`), filename.replace(/\.mdx$/,'')]))
-  const entries = metadata.map(([moduleExport, moduleName]) => ({metadata: moduleExport.meta, id: moduleName}))
+  const metadata = await Promise.all(filenames.map(async (filename) => [await require(`./entries/${filename}`), filename.replace(/\.mdx$/, '')]))
+  const entries = metadata.map(([moduleExport, moduleName]) => ({ metadata: moduleExport.meta, id: moduleName }))
 
   // const entryModules = await Promise.all(
   //   filenames.map(async (filename) => [import(`./entries/${filename}`), filename.replace(/\.mdx$/,'')])
